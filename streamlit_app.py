32dfa63c4762a3676090442f13c9b627eb7d020b
Daniel-dg-conta1 <123555598+Daniel-dg-conta1@users.noreply.github.com>
2023-01-25 19:23:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ ticker="CCRO3.SA"
 df=yf.download(ticker, period="10y", auto_adjust= True)
 
 
-arr = np.random.normal(1, 1, size=1000)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=30)
+#arr = np.random.normal(1, 1, size=1000)
+fig = plt.subplots()
+ax.plot(df['Close'])
 
 st.pyplot(fig)
