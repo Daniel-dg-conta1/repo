d793dbc41dfb90a44fc2d6a372daff94af2299d3
Daniel-dg-conta1 <123555598+Daniel-dg-conta1@users.noreply.github.com>
2023-01-25 19:21:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,21 +9,8 @@ import plotly.figure_factory as ff
 #df=yf.download(ticker, period="10y", auto_adjust= True)
 
 
-#st.pyplot(plt.plot(df["Close"]))
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
+st.pyplot(fig)
