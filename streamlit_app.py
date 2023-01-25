00c0f53c288a09809cbb9fa65f17c32d0fef91cb
Daniel-dg-conta1 <123555598+Daniel-dg-conta1@users.noreply.github.com>
2023-01-25 19:09:43 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
+import streamlit as st
 
 ticker="CCRO3.SA"
 df=yf.download(ticker, period="10y", auto_adjust= True)
