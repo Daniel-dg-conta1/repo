607016f42482f767cc93075429dc5af9fe1215cf
Daniel-dg-conta1 <123555598+Daniel-dg-conta1@users.noreply.github.com>
2023-01-25 15:41:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader.data as web
+import yfinance as yf
+yf.pdr_override()
+
+
 
 
 st.sidebar.title('menu')
@@ -9,3 +17,7 @@ if paginaSelecionada== 'Página 1':
     st.selectbox('Selecione uma opção',['opção 1', 'opção 2'])
 elif paginaSelecionada== 'Página 2':
     st.title('Você selecionou a página 2')
+    
+
+papel=yf.download('PETR4.SA', period="10y", auto_adjust= True)
+st.pyplot(papel["Close"].plot(figsize=(22,8)))
