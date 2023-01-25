c85c5692d81b6912eb86533213bdc88dfd0c21b6
Daniel-dg-conta1 <123555598+Daniel-dg-conta1@users.noreply.github.com>
2023-01-25 14:23:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
-streamlit run streamlit_app.py [-- script args]
+!pip install streamlit
+!pip install yfinance --upgrade --no-cache-dir
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader.data as web
+import yfinance as yf
+yf.pdr_override()
+papel=yf.download('PETR4.SA')
+papel["Close"].plot(figsize=(22,8))
