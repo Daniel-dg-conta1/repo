b57ee666971f90935a23ff60ce738c598a66c32b
Daniel-dg-conta1 <123555598+Daniel-dg-conta1@users.noreply.github.com>
2023-01-25 21:23:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,9 @@ def main():
     stock_lastprice = stock_data.history(period='1d', start=today, end=today)
     #get current date closing price for searched ticker
     #last_price = (stock_lastprice.Close)
-    last_price = round(stock_df['Close'].iloc[-1],2)
-    st.write('Data:' + today() + ' Preço: " + last_price)
+    last_price = str(round(stock_df['Close'].iloc[-1],2))
+    hoje=str(stock_df.index[-1].strftime('%d/%m/%y'))
+    st.write('Data:' + hoje + ' Preço: " + last_price)
     
     #last_price = '22.35'
     #if market is closed on current date print that there is no data available
